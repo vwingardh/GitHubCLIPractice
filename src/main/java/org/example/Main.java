6a651432c41281e58951704a6a83113349caa849
Vanessa Wingårdh <vwingardh@gmail.com>
2023-05-19 10:23:07 +0200

    Created scanner and JSON objects to collect user information

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import org.json.JSONObject;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(getInformation());
+    }
+
+    public static String getInformation() {
+        JSONObject json = new JSONObject(); // Create a JSON object
+        Scanner scan = new Scanner(System.in);  // Create a Scanner object
+
+        System.out.println("Enter your first name: ");
+        String userFirstName = scan.nextLine();  // Read user input
+        json.put("first_name", userFirstName);
+
+        System.out.println("Enter your last name: ");
+        String userLastName = scan.nextLine();
+        json.put("last_name", userLastName);
+
+        System.out.println("Enter your age: ");
+        String userAge = scan.nextLine();
+        json.put("age", userAge);
+
+        scan.close(); // Close scanner
+
+        return json.toString(); // return JSON string to main
     }
 }
